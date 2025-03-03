91de53189296673b00f522686c52d65fe7f8fae9
Ali Saleh <156487348+Xhaelri@users.noreply.github.com>
2025-03-03 03:05:24 +0200

    fixed account page

--- vite-project/src/pages/ProtectedRoute.jsx
+++ vite-project/src/pages/ProtectedRoute.jsx
@@ -4,8 +4,6 @@ import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const user = useSelector((state) => state.auth.user);
-  console.log("User in ProtectedRoute:", user);
-  getProductById;
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
